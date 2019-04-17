3310b499850cb8542a9c9c0a651734d0973c7439
shivammishra217 <shivammishra009@gmail.com>
2019-04-17 23:18:33 +0530

    initial version

---
+++ src/main/java/com/spring/experiments/ExperimentalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.experiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentalProjectApplication.class, args);
+	}
+
+}
